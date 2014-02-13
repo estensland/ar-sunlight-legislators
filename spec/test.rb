72bdb4f0dd7df762b5e789d3e7f0b1feea8c17fe
Apprentice <apprentice@dbc-chicago-013.local>
2014-02-13 14:40:20 -0600

    refacotring to more active record commands is needed

---
+++ spec/test.rb
@@ -0,0 +1,33 @@
+require_relative '../app/models/congress_member'
+
+require_relative '../db/config'
+
+p CongressMember.all.length
+
+# Get all senators:
+# CongressMember.where(title: "Sen").each do |senator|
+#   p "#{senator.firstname} #{senator.lastname} (#{senator.party})"
+# end
+
+# Get all senators:
+# CongressMember.where(state: "Sen").each do |senator|
+#   p "#{senator.firstname} #{senator.lastname} (#{senator.party})"
+# end
+
+# Get all congressmen_(How bout Illinois?):
+# CongressMember.find_by_state("IL")
+
+# p CongressMember.where(in_office: 1)
+
+# CongressMember.find_active_legislators("male")
+
+CongressMember.states
+
+puts
+
+CongressMember.total_numbers
+
+puts
+
+
+CongressMember.total_active
